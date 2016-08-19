6742f62d167d8a669b13b4cb5cf3b6082a07b7ab
SelvaGanesh M <selvaganesh.developer@gmail.com>
2016-08-19 11:49:06 +0530

    Initial commit

---
+++ app/src/main/java/com/internetconnection_demo/InternetConnector_Receiver.java
@@ -0,0 +1,66 @@
+package com.internetconnection_demo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class InternetConnector_Receiver extends BroadcastReceiver {
+    public InternetConnector_Receiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+
+            boolean isVisible = MyApplication.isActivityVisible();
+            Log.i("Activity is Visible ", "Is activity visible : " + isVisible);
+
+            // If it is visible then trigger the task else do nothing
+            if (isVisible == true) {
+                ConnectivityManager connectivityManager = (ConnectivityManager) context
+                        .getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager
+                        .getActiveNetworkInfo();
+
+                // Check internet connection and accrding to state change the
+                // text of activity by calling method
+                if (networkInfo != null && networkInfo.isConnected()) {
+
+                    new MainActivity().changeTextStatus(true);
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+
+                    try {
+                        URL url = new URL("http://www.google.com");
+                        HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+                        urlc.setConnectTimeout(3000);
+                        urlc.connect();
+                        if (urlc.getResponseCode() == 200) {
+                            new MainActivity().changeTextStatus(true);
+                        }
+                    } catch (MalformedURLException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                } else {
+                    new MainActivity().changeTextStatus(false);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
